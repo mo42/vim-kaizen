bc75e53aedc43ea00fecdbbd04e9494715ba5ce2
Moritz Winter <me@mo42.de>
2025-02-11 22:03:33 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # vim-kaizen
-Continous improvement in NeoVim
+
+`vim-kaizen` is a NeoVim plugin to encourage continuous improvement in your Vim keybindings.
+It reminds you to use alternative and better keys for increased productivity and efficiency.
+
+## Features
+
+- Reminds you to unlearn inefficient commands.
+- Provides suggestions for better commands.
+
+![vim-kaizen example in Vim editor](.example.png)
+
+## Installation
+
+#### `vim-plug`
+
+```vim
+Plug 'mo42/vim-kaizen'
+```
---
+++ plugin/vim-kaizen.lua
@@ -0,0 +1,38 @@
+function VimKaizen(pat, alt)
+  local txt = string.format('You entered "%s", which is an anti-pattern. Consider using "%s" from now on.', pat, alt)
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {txt})
+  local ui = vim.api.nvim_list_uis()[1] or vim.api.nvim_list_uis()[0]
+
+  local width = 50
+  local height = 10
+  local opts = {
+      relative = 'editor',
+      width = width,
+      height = height,
+      col = (ui.width / 2) - (width / 2),
+      row = (ui.height / 2) - (height / 2),
+      anchor = 'NW',
+      style = 'minimal',
+      border = 'shadow'
+  }
+
+  local win = vim.api.nvim_open_win(buf, true, opts)
+  vim.api.nvim_set_current_win(win)
+  vim.api.nvim_buf_set_keymap(buf, 'n', '<Esc>', '<Cmd>bwipeout!<CR>', {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<Cmd>bwipeout!<CR>', {noremap = true, silent = true})
+end
+
+vim.api.nvim_create_user_command('VimHabits', function(opts) VimHabits(opts.args) end, {nargs=1})
+
+local vimKaizenPatterns = {
+    {'n', 'd$', 'D'},
+    {'n', 'y$', 'Y'},
+    {'n', 'ggVG', 'yG'},
+    {'n', 'cc', 'S'}
+}
+
+for _, tuple in ipairs(vimKaizenPatterns) do
+  local map, pat, alt = table.unpack(tuple)
+  vim.keymap.set(map, pat, function() VimKaizen(pat, alt) end, { noremap = true, silent = true })
+end
